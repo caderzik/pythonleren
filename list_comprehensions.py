b66017ac87b1300f92e0d6ae8019c0603bce74ab
caderzik <cinemannde@gmail.com>
2024-04-18 20:45:01 +0200

    mega

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     n = int(input())
 
 
-summe = [[i, j, k] for i in range(x) for j in range(y) for k in range(z) if x+y+z != n]
-print(summe)
+
+print([[i, j, k] for i in range(0,x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n])
